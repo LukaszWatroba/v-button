effa9516394519cd61f79ce60b0d3be3de2c24ea
LukaszWatroba <lukasz.jan.watroba@gmail.com>
2015-01-14 16:40:05 +0100

    disable debug info

--- demo/app.js
+++ demo/app.js
@@ -4,6 +4,10 @@
   angular
     .module('myApp', [ 'vButton' ])
 
+    .config(function ($compileProvider) {
+      $compileProvider.debugInfoEnabled(false);
+    })
+
     .controller('MainController', function () { });
 
 })(angular);
